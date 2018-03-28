8919ad756aba7e4c112170bace5207fa08e2e6f3
Aaron Ault <ault@purdue.edu>
2018-03-28 15:45:56 -0400

    ignoring produciton build files

--- aultfarms-lib/trello/module/errors.js
+++
@@ -1,6 +0,0 @@
-import { CerebralError } from 'cerebral';
-
-export class TrelloSaveError extends CerebralError {};
-export class TrelloGetError extends CerebralError {};
-export class TrelloAuthorizeError extends CerebralError {};
-//# sourceMappingURL=errors.js.map
