8177adb8925d64a8bcfea07e845487346cb07118
Aaron Ault <ault@purdue.edu>
2018-03-28 19:56:44 -0400

    finally got package to work

---
+++ aultfarms-lib/src/index.js
@@ -0,0 +1,19 @@
+import * as util            from 'util';
+import * as dead            from 'dead';
+import * as google          from 'google';
+import * as trello          from 'trello';
+import * as window          from 'window';
+import * as incoming        from 'incoming';
+import * as treatments      from 'treatments';
+import * as cattleInventory from 'cattleInventory';
+
+export {
+  util,
+  dead,
+  google,
+  trello,
+  window,
+  incoming,
+  treatments,
+  cattleInventory,
+};
