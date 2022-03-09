63892c9a7357ff6e3a2b24412d08b41d336e6986
Aaron Ault <aultac@gmail.com>
2022-03-09 12:59:57 -0500

    MVP

---
+++ apps/accounts/src/state/initialize.ts
@@ -0,0 +1,117 @@
+
+import { action } from 'mobx';
+import { state, IndexedStatements } from './state';
+import { activity, errors, stepResult, balancesheets, profitlosses } from './actions';
+import debug from 'debug';
+import { ledger as ledger, balance, profitloss, google } from '@aultfarms/accounts';
+
+const info= debug("accounts#initialize:info");
+const warn = debug("accounts#initialize:warn");
+
+export const onInitialize = action('onInitialize', async () => {
+
+  if (state.config.accountsLocation.place !== 'google') {
+    activity('ERROR: any place other that google is not currently supported');
+    return;
+  }
+
+  // Read the accounts:
+  let rawaccts = null;
+  try {
+    rawaccts = await google.readAccountsFromGoogle({ 
+      accountsdir: state.config.accountsLocation.path, 
+      status: activity
+    });
+  } catch(e) {
+    warn('Could not readAccountsFromGoogle.  Error was: ', e);
+    activity('Error: Could not read accounts from google', 'bad');
+    return;
+  }
+
+  // Process them into a ledger:
+  let step;
+  try {
+    for await (step of ledger.loadInSteps({ rawaccts, status: activity })) {
+      if (step && step.errors) {
+        activity('ERROR ON STEP: '+step.step, 'bad');
+        break;
+      }
+    }
+    if (!step) {
+      activity('ERROR: no step!', 'bad');
+      return;
+    }
+    stepResult(step);
+    if (step.errors && step.errors.length > 0) {
+      activity(step.errors, 'bad');
+      errors(step.errors);
+      return;
+    }
+    if (!step.done) {
+      activity('Did not finish loading', 'bad');
+      return;
+    }
+    if (!step.final) {
+      activity('Finished loading, but had no accounts!');
+      return;
+    }
+    activity('Finished loading ledger, on to balance sheets and profit/loss statements...');
+  } catch (e: any) {
+    warn('Could not process accounts into ledger, error = ', e);
+    if (typeof e.msgs === 'function') {
+      activity(e.msgs(), 'bad');
+      errors(e.msgs());
+      
+    } else if (typeof e.message === 'string') {
+      activity(e.message, 'bad');
+    }
+    activity('Error: could not process accounts into ledger', 'bad');
+    return;
+  }
+
+  const final = step?.final;
+  if (!final) {
+    warn('WARNING: steps all finished, but final is still falsey....');
+    activity('ERROR: all ledger loading steps finished, but had no resulting accounts!');
+    return;
+  }
+  activity('Finished putting ledger into the state, on to balance sheets and profit/loss statements...');
+
+  // Create balance sheets and P&L for every year from 2020 forward
+  let bss: IndexedStatements<balance.AnnualBalanceSheet> = {}; 
+  let pls: IndexedStatements<profitloss.ProfitLoss> = {};
+  try {
+    const now = (new Date()).getFullYear();
+    info('now = ', now);
+    activity('******** balance sheet and profit loss ********');
+    for (let year=2020; year <= now; year++) {
+      activity(`Creating balance sheet for year ${year}`);
+      bss[year] = {
+        tax: await balance.annualBalanceSheet({ year, type: 'tax', ledger: final }),
+        mkt: await balance.annualBalanceSheet({ year, type: 'mkt', ledger: final }),
+      };
+      activity(`Creating profit/loss statement for year ${year}`);
+      pls[year] = {
+        tax: profitloss.profitLoss({ year, type: 'tax', ledger: final }),
+        mkt: profitloss.profitLoss({ year, type: 'mkt', ledger: final }),
+      };
+    }
+    balancesheets(bss);
+    profitlosses(pls);
+    activity('Successfully created balance sheet and profit/loss statements');
+
+  } catch(e: any) {
+    warn('Could not create balance sheets and P&L statements, error = ', e);
+    if (typeof e.msgs === 'function') {
+      activity(e.msgs(), 'bad');
+      errors(e.msgs());
+      
+    } else if (typeof e.message === 'string') {
+      activity(e.message, 'bad');
+    }
+    activity('Error: could not load accounts', 'bad');
+    return;
+  }
+
+});
+
---
+++ apps/accounts/src/state/util.ts
@@ -0,0 +1,47 @@
+import type { ActivityMessage } from './state';
+
+export function linenoPrefix(str: string): string | null {
+  const lineparts = str.match(/^ACCOUNT [^:]+: LINE: ([0-9]+):/);
+  return lineparts?.[1] || null;
+}
+
+export function accountPrefix(str: string) : string | null {
+  const accountparts = str.match(/^ACCOUNT ([^:]+):/);
+  return accountparts?.[1] || null;
+}
+
+export function combinePrefixedMsgs(errs: string[] | ActivityMessage[]): typeof errs {
+  if (errs.length < 1) return ([] as typeof errs);
+
+  // Force everything into activitymessages to make TS happy:
+  let amerrors: ActivityMessage[] = [];
+  for (const e of errs) {
+    if (typeof e === 'string') amerrors.push({ msg: e, type: 'good' });
+    else amerrors.push(e);
+  }
+
+  // Check prefixes, accumulating in an index if matched:
+  const nones: ActivityMessage[] = [];
+  const prefixed: { [prefix: string]: ActivityMessage } = {};
+  for (const e of amerrors) {
+    const pfx = linenoPrefix(e.msg) || accountPrefix(e.msg);
+    if (!pfx) {
+      nones.push(e);
+    } else {
+      if (!prefixed[pfx]) prefixed[pfx] = { msg: '', type: e.type };
+      prefixed[pfx]!.msg += `${e.msg}\n`;
+    }
+  }
+  
+  if (typeof errs[0] === 'string') {
+    return ([ 
+      ...nones.map(a => a.msg), 
+      ...Object.values(prefixed).map(a => a.msg)
+    ] as string[]);
+  } else {
+    return ([ 
+      ...nones, 
+      ...Object.values(prefixed) 
+    ] as ActivityMessage[]);
+  }
+}
