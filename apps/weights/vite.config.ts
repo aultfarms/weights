4fac291f1d5efcfc68b540ec7a370a1fdbb12f74
Aaron Ault <aultac@gmail.com>
2025-02-04 11:22:25 -0500

    Had to fix weights vite config too for base url to change back to weights

--- apps/weights/vite.config.ts
+++ apps/weights/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/af-monorepo/',
+  base: '/weights/',
   plugins: [react(), /*basicSsl()*/],
   server: {
     open: true,
